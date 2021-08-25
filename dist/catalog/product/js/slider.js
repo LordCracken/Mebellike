77630ac01aecfbb86cbcb4c6bfed2f58fecb5b3a
LordCracken <vladislav.yakimovskiy@mail.ru>
2021-08-25 19:55:42 +0300

    removed dist folder from repository

--- dist/catalog/product/js/slider.js
+++
@@ -1,1 +0,0 @@
-$(document).ready(function(){$(".product__slider").slick({slidesToShow:1,slidesToScroll:1,asNavFor:".product__nav",prevArrow:$(".prev-arrow"),nextArrow:$(".next-arrow")}),$(".product__nav").slick({slidesToShow:3,focusOnSelect:!0,asNavFor:".product__slider",useTransform:!1})});
